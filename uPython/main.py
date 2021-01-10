cafbdb3bd97c7938244268f67f15746df059beb9
David <39265971+somervda@users.noreply.github.com>
2021-01-09 19:41:50 -0500

    ADS1115 working

--- uPython/main.py
+++ uPython/main.py
@@ -2,6 +2,7 @@ import time
 from machine import I2C
 import pycom
 import bme280
+from ADS1115 import ADS1115
 
 
 def blink(seconds, rgb):
@@ -15,12 +16,16 @@ pycom.heartbeat(False)
 
 i2c = I2C(0, I2C.MASTER, pins=('P10', 'P9'), baudrate=100000)
 bme = bme280.BME280(i2c=i2c)
+# Defailt address for adc is 0x48 (72 dec)
+adc = ADS1115(i2c, address=0x48)
 
 for i in range(1):
     scan = i2c.scan()
     print("scan", i, " - ", scan)
     if len(scan) > 0:
-        print("bme:", bme.formated_values)
+        print("\nChannel 0 voltage: {}V".format(adc.get_voltage(0)))
+        print("Channel 0 ADC value: {}\n".format(adc.read(0)))
+        # print("bme:", bme.formated_values)
         blink(1, 0xffffff)  # blink white
     else:
         blink(1, 0xff0000)  # blink red
